f8028a0338c588cc29880cf0a51a5e6476cd153a
Aslan3310 <aslan070105@gmail.com>
2025-02-01 21:09:09 +0500

    +pausemenu

---
+++ assets/scenes/pause.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		visible = true
+
+func _on_continue_pressed() -> void:
+	visible = false
+
+
+func _on_options_pressed() -> void:
+	$settings.visible = true
+
+
+func _on_quit_pressed() -> void:
+	var mainMenu = "res://assets/scenes/main.tscn"
+	get_tree().change_scene_to_file(mainMenu)
